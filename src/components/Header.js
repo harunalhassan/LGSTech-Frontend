8a4b0ba982f39a35bea6e8204218aa79c8777e9f
harunalhassan <128882187+harunalhassan@users.noreply.github.com>
2025-10-31 18:17:52 +0530

    logo changed and preloader modifi done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import '../styles/Header.css';
 // import logo from "../assets/final LGSTech logo w2.png"
-import logo from "../assets/primaryLGS_logo/LGS Tech 3D logo_cropped.JPG"
+import logo from "../assets/primaryLGS_logo/LGS Tech 3D logo_cropped.jpg"
 // import logo from "../assets/primaryLGS_logo/LGSTECH Logo White Background_cropped.JPG"
 
 function Header() {
